26e8de19ac6a027ad34c2519c38dea4b32b0ab1f
HemaLatha <hema.modem@capgemini.com>
2020-04-18 22:46:25 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Clock from './components/Clock'
+import Header from './components/Header'
+
+export default class App extends Component {
+
+  
+    constructor() {
+        super();
+
+        this.state = {
+            title: 'React Clock'
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Header title={this.state.title} />
+                <Clock />
+            </div>
+        );
+    }
 }
-
-export default App;
